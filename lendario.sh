cd120c370743c3d505159ffafaa1209350fcd144
Seiva_Android_OPPO_A16s_Mike_2 <flowreshe.seiva.d.arve@gmail.com>
2025-10-03 21:46:27 +0100

    Pushed to github.com automatically (by ezGIT app)

--- lendario.sh
+++ lendario.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 __name__="Lendario"  # Change to the name of the script. Example: DRYa.sh, ezGIT.sh, Patuscas.sh (Set this variable at the head of the file, next to title)
+__repo__=${v_REPOS_CENTER}/Cao-Lendario
 
 # Sourcing DRYa Lib 1: Color schemes
    __name__="Lendario"  # Change to the name of the script. Example: DRYa.sh, ezGIT.sh, Patuscas.sh (Set this variable at the head of the file, next to title)
@@ -14,3 +15,20 @@ f_talk; echo "Mes atual"
         echo
 
 cal
+
+
+
+
+if [ -z $1 ]; then
+   echo "none"
+
+elif [ $1 == "." ]; then
+   bash e $__repo__/lendario.sh
+
+elif [ $1 == "2" ] || [ $1 == "3" ]; then
+   echo "2 or 3"
+
+else
+   echo "uDev: help"
+
+fi
